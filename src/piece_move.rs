4906cdb7ece013f9638e0f32f151d989f29e771e
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2025-07-16 23:00:48 +0100

    Move Castling Handling to Seperate Function Beginning to add Promotion

--- src/piece_move.rs
+++ src/piece_move.rs
@@ -6,7 +6,7 @@ use crate::{
     board::{Board, TilePos},
     checkmate::CheckmateEvent,
     display::{board_to_pixel_coords, BackgroundColourEvent, BOARD_SIZE},
-    piece::Piece,
+    piece::{Piece, COLOUR_AMT},
     possible_moves::get_possible_moves,
 };
 
@@ -22,6 +22,10 @@ pub enum PieceMoveType {
     Normal,
     EnPassant,
     Castling,
+    Promotion {
+        from_piece: Piece,
+        to_piece: Piece,
+    },
 }
 
 #[derive(Clone, Copy, PartialEq, Eq)]
@@ -69,6 +73,19 @@ impl PieceMove {
             from: self.from,
             to: self.to,
             move_type: PieceMoveType::EnPassant,
+            show: self.show,
+        }
+    }
+
+    #[must_use]
+    pub const fn with_promotion(&self, from_piece: Piece, to_piece: Piece) -> Self {
+        Self {
+            from: self.from,
+            to: self.to,
+            move_type: PieceMoveType::Promotion {
+                from_piece,
+                to_piece,
+            },
             show: self.show,
         }
     }
@@ -164,6 +181,8 @@ pub fn piece_move_event_handler(
 
                 let moved_piece = board.get_piece(piece_move.from);
 
+                // Handle promotion
+
                 // Handle en passant, if this move is en passant, or if this move allows en passant on the next move
                 let en_passant_tile;
                 (en_passant_tile, piece_move, piece_captured, piece_moved_to) = handle_en_passant(
@@ -175,86 +194,10 @@ pub fn piece_move_event_handler(
                     piece_moved_to,
                 );
 
-                // Remember the castling rights before this move
-                let castling_rights_before_move = board.castling_rights;
-
-                // Handle castling rights
-                {
-                    let player_index = board.get_player().to_index();
-
-                    // Only update if the castling rights aren't already false
-                    if board.castling_rights[player_index] != (false, false) {
-                        // King was moved
-                        if moved_piece == board.get_player_king(board.get_player()) {
-                            board.castling_rights[player_index] = (false, false);
-                        }
-                        // Rook was moved
-                        else if moved_piece
-                            == board.get_player_piece(board.get_player(), Piece::WRook)
-                        {
-                            // Kingside
-                            if piece_move.from.file == BOARD_SIZE - 1 {
-                                board.castling_rights[player_index].0 = false;
-                            }
-                            // Queenside
-                            else if piece_move.from.file == 0 {
-                                board.castling_rights[player_index].1 = false;
-                            }
-                        }
-                    }
-                }
-
                 // Handle Castling
-                // If piece is this player's king, and the king moved 2 spaces
-                let file_diff_isize = isize::try_from(piece_move.to.file).unwrap()
-                    - isize::try_from(piece_move.from.file).unwrap();
-                if moved_piece == board.get_player_king(board.get_player())
-                    && file_diff_isize.unsigned_abs() == 2
-                {
-                    piece_move = piece_move.with_castling();
-
-                    fn move_rook_for_castle(
-                        board: &mut Board,
-                        transform_query: &mut Query<&mut Transform>,
-                        file: usize,
-                        new_file: usize,
-                        from_rank: usize,
-                    ) {
-                        let rook_pos = TilePos::new(file, from_rank);
-                        let new_rook_pos = TilePos::new(new_file, rook_pos.rank);
-
-                        // Move the rook entity
-                        translate_piece_entity(
-                            board
-                                .get_entity(rook_pos)
-                                .expect("Rook entity was not at Rook pos"),
-                            new_rook_pos,
-                            transform_query,
-                        );
-
-                        // Move the rook (and its entity ID) internally
-                        board.move_piece(PieceMove::new(rook_pos, new_rook_pos));
-                    }
-
-                    // Kingside Castle
-                    if file_diff_isize > 0 {
-                        move_rook_for_castle(
-                            &mut board,
-                            &mut transform_query,
-                            BOARD_SIZE - 1,
-                            BOARD_SIZE - 3,
-                            piece_move.from.rank,
-                        );
-                    } else {
-                        move_rook_for_castle(
-                            &mut board,
-                            &mut transform_query,
-                            0,
-                            3,
-                            piece_move.from.rank,
-                        );
-                    }
-                }
+                let castling_rights_before_move;
+                (castling_rights_before_move, piece_move) =
+                    handle_castling(&mut board, &mut transform_query, piece_move, moved_piece);
 
                 let captured_piece = if piece_captured {
                     Some(piece_moved_to)
@@ -350,4 +293,85 @@ fn handle_en_passant(
     }
 
     (en_passant_tile, piece_move, piece_captured, piece_moved_to)
+}
+
+pub fn handle_castling(
+    board: &mut Board,
+    transform_query: &mut Query<&mut Transform>,
+    mut piece_move: PieceMove,
+    moved_piece: Piece,
+) -> ([(bool, bool); COLOUR_AMT], PieceMove) {
+    // Remember the castling rights before this move
+    let castling_rights_before_move = board.castling_rights;
+
+    // Handle castling rights
+    {
+        let player_index = board.get_player().to_index();
+
+        // Only update if the castling rights aren't already false
+        if board.castling_rights[player_index] != (false, false) {
+            // King was moved
+            if moved_piece == board.get_player_king(board.get_player()) {
+                board.castling_rights[player_index] = (false, false);
+            }
+            // Rook was moved
+            else if moved_piece == board.get_player_piece(board.get_player(), Piece::WRook) {
+                // Kingside
+                if piece_move.from.file == BOARD_SIZE - 1 {
+                    board.castling_rights[player_index].0 = false;
+                }
+                // Queenside
+                else if piece_move.from.file == 0 {
+                    board.castling_rights[player_index].1 = false;
+                }
+            }
+        }
+    }
+
+    // If piece is this player's king, and the king moved 2 spaces
+    let file_diff_isize = isize::try_from(piece_move.to.file).unwrap()
+        - isize::try_from(piece_move.from.file).unwrap();
+    if moved_piece == board.get_player_king(board.get_player())
+        && file_diff_isize.unsigned_abs() == 2
+    {
+        piece_move = piece_move.with_castling();
+
+        fn move_rook_for_castle(
+            board: &mut Board,
+            transform_query: &mut Query<&mut Transform>,
+            file: usize,
+            new_file: usize,
+            from_rank: usize,
+        ) {
+            let rook_pos = TilePos::new(file, from_rank);
+            let new_rook_pos = TilePos::new(new_file, rook_pos.rank);
+
+            // Move the rook entity
+            translate_piece_entity(
+                board
+                    .get_entity(rook_pos)
+                    .expect("Rook entity was not at Rook pos"),
+                new_rook_pos,
+                transform_query,
+            );
+
+            // Move the rook (and its entity ID) internally
+            board.move_piece(PieceMove::new(rook_pos, new_rook_pos));
+        }
+
+        // Kingside Castle
+        if file_diff_isize > 0 {
+            move_rook_for_castle(
+                board,
+                transform_query,
+                BOARD_SIZE - 1,
+                BOARD_SIZE - 3,
+                piece_move.from.rank,
+            );
+        } else {
+            move_rook_for_castle(board, transform_query, 0, 3, piece_move.from.rank);
+        }
+    }
+
+    (castling_rights_before_move, piece_move)
 }
